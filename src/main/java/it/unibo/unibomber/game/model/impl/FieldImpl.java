17573068b5ceae3c03aaadf7fc04d62dda0833f0
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-06 14:00:52 +0100

    Created utilities folder and created Field methods

--- src/main/java/it/unibo/unibomber/game/model/impl/FieldImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/FieldImpl.java
@@ -1,7 +1,26 @@
 package it.unibo.unibomber.game.model.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unibo.unibomber.game.ecs.api.Entity;
+import it.unibo.unibomber.game.ecs.api.Type;
 import it.unibo.unibomber.game.model.api.Field;
+import it.unibo.unibomber.utilities.Pair;
 
 public class FieldImpl implements Field{
+
+    private final Map<Pair<Integer, Integer>, Pair<Type, Entity>> field = new HashMap<>();
+
+    @Override
+    public Map<Pair<Integer, Integer>, Pair<Type, Entity>> getField() {
+        return new HashMap<>(this.field);
+    }
+
+    @Override
+    public void updateField() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateField'");
+    }
     
 }
