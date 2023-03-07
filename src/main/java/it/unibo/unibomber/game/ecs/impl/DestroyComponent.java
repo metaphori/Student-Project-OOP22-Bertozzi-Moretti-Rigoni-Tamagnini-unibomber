638339accb6e8caa47bd8efe22fc5b27c7b9d764
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-07 11:26:05 +0100

    Completed updateField method in FieldImpl

--- src/main/java/it/unibo/unibomber/game/ecs/impl/DestroyComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/DestroyComponent.java
@@ -1,13 +1,5 @@
 package it.unibo.unibomber.game.ecs.impl;
 
-import java.util.List;
-import java.util.Random;
-
-import it.unibo.unibomber.game.ecs.api.Entity;
-import it.unibo.unibomber.game.ecs.api.PowerUpType;
-import it.unibo.unibomber.game.model.api.Game;
-import it.unibo.unibomber.utilities.Pair;
-
 /**
  * This component manage the destroy of the entity
  */
@@ -21,56 +13,18 @@ public class DestroyComponent extends AbstractComponent{
     @Override
     public void update() {
         if (this.isDestroyed) {
-            if (this.destroyFrames-- == 0) {
-                dropPowerUps();
+            if (this.destroyFrames == 0) {
                 this.getEntity().getGame().removeEntity(this.getEntity());
+                //TODO: add powerup drop manage
+                this.isDestroyed = false;
+            } else {
+                this.destroyFrames--;
             }
-        }
-    }
-    
-    public void dropPowerUps(){
-        Entity entity = this.getEntity();
-        var entityPowerupComponent = entity.getComponent(PowerUpListComponent.class);
-        if(entityPowerupComponent.isPresent()){
-            List<PowerUpType> entityPowerups = entityPowerupComponent.get().getPowerUpList();
-            int droppedEntities = (int)Math.ceil(entityPowerups.size()*droppedPowerUpsPercent);
-            dropFirstEntity(entityPowerups);
-            dropRemainingPowerup(entityPowerups, droppedEntities);
         }
     }
 
     public void destroy() {
         this.isDestroyed = true;
-    }
-
-    private void dropRemainingPowerup(List<PowerUpType> entityPowerups, int droppedEntities) {
-        while(entityPowerups.size()>droppedEntities){
-            entityPowerups.remove((int)(Math.random()*entityPowerups.size()));
-        }
-        Game thisGame = this.getEntity().getGame();
-        for(PowerUpType powerUp : entityPowerups){
-            Pair<Integer,Integer> dimensions = this.getEntity().getGame().getDimensions();
-            Pair<Float,Float> newPosition = getLegalPosition(dimensions);
-            thisGame.addEntity(thisGame.getFactory().makePowerUp(newPosition, powerUp));
-        }
-    }
-
-    private Pair<Float, Float> getLegalPosition(Pair<Integer, Integer> dimensions) {
-        final Random rnd = new Random();
-        Pair<Float,Float> newPosition;
-        do{
-            newPosition=new Pair<Float,Float>
-                        ((float)rnd.nextInt(dimensions.getX()), (float)rnd.nextInt(dimensions.getY()));
-        }while(this.getEntity().getGame().getGameField().getField().containsKey(newPosition));
-
-        return newPosition;
-    }
-
-    private void dropFirstEntity(List<PowerUpType> entityPowerups) {        
-        Entity entity = this.getEntity();
-        entity.getGame().addEntity(entity.getGame().getFactory()
-              .makePowerUp(entity.getPosition(), entityPowerups.get(0)));
-        entityPowerups.remove(0);
     }
 
 }
