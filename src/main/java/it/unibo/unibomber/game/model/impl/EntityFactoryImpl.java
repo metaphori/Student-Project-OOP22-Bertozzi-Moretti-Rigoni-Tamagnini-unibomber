934a5c62bdf7b9dc176922e0d26c8998a3758ce4
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2023-03-07 12:03:12 +0100

    Updated EntityFactoryImpl

--- src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
@@ -1,29 +1,39 @@
 package it.unibo.unibomber.game.model.impl;
 
+import java.util.List;
+
 import it.unibo.unibomber.game.ecs.api.Entity;
 import it.unibo.unibomber.game.ecs.api.PowerUpType;
 import it.unibo.unibomber.game.ecs.api.Type;
+import it.unibo.unibomber.game.ecs.impl.DestroyComponent;
+import it.unibo.unibomber.game.ecs.impl.EntityImpl;
+import it.unibo.unibomber.game.ecs.impl.PowerUpComponent;
+import it.unibo.unibomber.game.ecs.impl.PowerUpListComponent;
 import it.unibo.unibomber.game.model.api.EntityFactory;
 import it.unibo.unibomber.game.model.api.Game;
 import it.unibo.unibomber.utilities.Pair;
 
 public class EntityFactoryImpl implements EntityFactory{
 
-    Game game;
+    private final  Game game;
+
     public EntityFactoryImpl(Game game){
-        this.game=game;
+        this.game = game;
     }
-    
+
     @Override
     public Entity makePowerUp(Pair<Float, Float> pos, PowerUpType powerUpType) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makePowerUp'");
+        return new EntityImpl(game, pos, Type.POWERUP)
+            .addComponent(new PowerUpComponent(powerUpType))
+            .addComponent(new DestroyComponent());
     }
 
     @Override
     public Entity makeBomber(Pair<Float, Float> position, Type type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeBomber'");
+        return new EntityImpl(game,position,type)
+            //TODO add other component
+            .addComponent(new PowerUpListComponent(1, 1, List.of()))
+            .addComponent(new DestroyComponent());  
     }
 
     @Override
