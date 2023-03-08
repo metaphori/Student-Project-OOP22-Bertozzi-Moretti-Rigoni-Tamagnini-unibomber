47a21fd81aa52f44c15267780d39f3fd22d56df7
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2023-03-08 08:50:04 +0100

    Completed  method addPowerUp

--- src/main/java/it/unibo/unibomber/game/ecs/impl/PowerUpHandlerComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/PowerUpHandlerComponent.java
@@ -4,7 +4,7 @@ import java.util.List;
 import it.unibo.unibomber.game.ecs.api.PowerUpType;
 
 public class PowerUpHandlerComponent extends PowerUpListComponent {
-    
+
     public PowerUpHandlerComponent(int bombNumber, int bombPower, List<PowerUpType> powerUpList) {
         super(bombNumber, bombPower, powerUpList);
     }
@@ -13,7 +13,34 @@ public class PowerUpHandlerComponent extends PowerUpListComponent {
      * @param powerUp that modify powerup parameter of player
      */
     public void addPowerUp(PowerUpType powerUpType) {
-        //TODO update value with controll
-
+        if (!powerUpType.isComplex()) {
+            switch (powerUpType) {
+                case FIREUP:
+                    if (this.bombPower < 8) {
+                        this.bombPower += 1;
+                    }
+                    break;
+                case FIREDOWN:
+                    if (this.bombPower > 1) {
+                        this.bombPower -= 1;
+                    }
+                    break;
+                case FIREFULL:
+                    this.bombPower = 8;
+                    break;
+                case BOMBUP:
+                    if (this.bombNumber < 8) {
+                        this.bombNumber += 1;
+                    }
+                    break;
+                case BOMBDOWN:
+                    if (this.bombNumber > 1) {
+                        this.bombNumber -= 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
