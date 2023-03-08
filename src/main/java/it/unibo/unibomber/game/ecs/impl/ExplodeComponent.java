2a14b09cece508e143a3c48d1a2243ba0c1af8ca
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-08 17:08:06 +0100

    Completed explodeComponent

--- src/main/java/it/unibo/unibomber/game/ecs/impl/ExplodeComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/ExplodeComponent.java
@@ -1,7 +1,10 @@
 package it.unibo.unibomber.game.ecs.impl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import it.unibo.unibomber.game.ecs.api.Entity;
 import it.unibo.unibomber.game.ecs.api.Type;
@@ -10,45 +13,60 @@ import it.unibo.unibomber.utilities.Pair;
 
 public class ExplodeComponent extends AbstractComponent {
 
+     private static int EXPLODE_DURATION = 5;
+     private int explodeFrames = 0;
      private boolean isExploding = false;
 
      @Override
      public void update() {
-          // TODO Auto-generated method stub
-          throw new UnsupportedOperationException("Unimplemented method 'update'");
+          if (this.isExploding) {
+               this.explodeFrames++;
+               if (this.explodeFrames < EXPLODE_DURATION) {
+                    explodeEntities(this.getEntity().getGame().getEntities().stream()
+                         .filter(e -> e.getType() == Type.BOT || e.getType() == Type.PLAYABLE)
+                         .collect(Collectors.toList()));
+               } else {
+                    this.getEntity().getGame().removeEntity(this.getEntity());
+                    this.explodeFrames = 0;
+                    this.isExploding = false;
+               }
+          }
      }
 
      /**
       * A method to destroy wall or powerups in the bomb range
       */
      public void explode() {
-          Pair<Float, Float> checkPos;
-          int bombRange = this.getEntity().getComponent(PowerUpListComponent.class).get().getBombPower();
-          List<Entity> entitiesList = this.getEntity().getGame().getEntities().stream()
-                              .filter(e -> e.getType() != Type.BOT && e.getType() != Type.PLAYABLE)
-                              .collect(Collectors.toList());
-          var field = this.getEntity().getGame().getGameField().getField();
-          int countPositions;
-          for (var entity : entitiesList) {
-               if (entity.getType() == Type.BOMB && this.isExploding) {
-                    for (var dir : Direction.values()) {
-                         countPositions = 1;
-                         while(countPositions <= bombRange) {
-                              checkPos = new Pair<Float,Float>(entity.getPosition().getX() + dir.getX() * countPositions, 
-                              entity.getPosition().getY() + dir.getY() * countPositions);
-                              if (field.containsKey(checkPos)) {
-                                   if (field.get(checkPos).getX() == Type.BOMB) {
-                                        explode();
-                                   } else {
-                                        this.getEntity().getComponent(DestroyComponent.class).get().destroy();
-                                   }
-                              }
-                              countPositions++;
-                         }
-                    }
-               }
-          }
+          explodeEntities(this.getEntity().getGame().getEntities().stream()
+                         .filter(e -> e.getType() != Type.BOT && e.getType() != Type.PLAYABLE)
+                         .collect(Collectors.toList()));
           this.isExploding = true;
      }
 
+     /**
+      * General method to control if there are some entities on the bomb range
+      * @param entitiesList the entities to control
+      */
+     private void explodeEntities(List<Entity> entitiesList) {
+          int bombRange = this.getEntity().getComponent(PowerUpListComponent.class).get().getBombPower();
+          var field = this.getEntity().getGame().getGameField().getField();
+          entitiesList.stream()
+              .forEach(entity -> {
+                  Arrays.stream(Direction.values()).forEach(dir -> {
+                      IntStream.rangeClosed(1, bombRange).mapToObj(countPositions -> {
+                          Pair<Float, Float> checkPos = new Pair<>(
+                              entity.getPosition().getX() + dir.getX() * countPositions,
+                              entity.getPosition().getY() + dir.getY() * countPositions
+                          );
+                          return field.get(checkPos);
+                      }).filter(Objects::nonNull).findFirst().ifPresent(type -> {
+                          if (type.getX() == Type.BOMB) {
+                              explodeEntities(entitiesList);
+                          } else {
+                              this.getEntity().getComponent(DestroyComponent.class).get().destroy();
+                          }
+                      });
+                  });
+              });
+     }
 }
