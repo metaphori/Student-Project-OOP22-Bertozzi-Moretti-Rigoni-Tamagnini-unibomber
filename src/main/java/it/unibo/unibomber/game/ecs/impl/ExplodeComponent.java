770c3de0ae00758397566fd7faf36c8a7d836cd8
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-03-08 10:58:05 +0100

    Added components to EntityFactory, created ExplodeComponent

---
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/ExplodeComponent.java
@@ -0,0 +1,11 @@
+package it.unibo.unibomber.game.ecs.impl;
+
+public class ExplodeComponent extends AbstractComponent {
+
+     @Override
+     public void update() {
+          // TODO Auto-generated method stub
+          throw new UnsupportedOperationException("Unimplemented method 'update'");
+     }
+
+}
--- src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
@@ -5,9 +5,14 @@ import java.util.List;
 import it.unibo.unibomber.game.ecs.api.Entity;
 import it.unibo.unibomber.game.ecs.api.PowerUpType;
 import it.unibo.unibomber.game.ecs.api.Type;
-import it.unibo.unibomber.game.ecs.impl.BombPlaceComponent;
+import it.unibo.unibomber.game.ecs.impl.AIComponent;
+import it.unibo.unibomber.game.ecs.impl.CollisionComponent;
 import it.unibo.unibomber.game.ecs.impl.DestroyComponent;
 import it.unibo.unibomber.game.ecs.impl.EntityImpl;
+import it.unibo.unibomber.game.ecs.impl.ExplodeComponent;
+import it.unibo.unibomber.game.ecs.impl.InputComponent;
+import it.unibo.unibomber.game.ecs.impl.MovementComponent;
+import it.unibo.unibomber.game.ecs.impl.BombPlaceComponent;
 import it.unibo.unibomber.game.ecs.impl.PowerUpComponent;
 import it.unibo.unibomber.game.ecs.impl.PowerUpListComponent;
 import it.unibo.unibomber.game.model.api.EntityFactory;
@@ -16,15 +21,13 @@ import it.unibo.unibomber.utilities.Pair;
 
 public class EntityFactoryImpl implements EntityFactory{
 
-    private final  Game game;
-
+    Game game;
     public EntityFactoryImpl(Game game){
-        this.game = game;
+        this.game=game;
     }
-
     @Override
     public Entity makePowerUp(Pair<Float, Float> pos, PowerUpType powerUpType) {
-        return new EntityImpl(game, pos, Type.POWERUP)
+        return new EntityImpl(game,pos, Type.POWERUP)
             .addComponent(new PowerUpComponent(powerUpType))
             .addComponent(new DestroyComponent());
     }
@@ -32,39 +35,45 @@ public class EntityFactoryImpl implements EntityFactory{
     @Override
     public Entity makeBomber(Pair<Float, Float> position, Type type) {
         return new EntityImpl(game,position,type)
-            //TODO add other component
-            .addComponent(new PowerUpListComponent(1, 1, List.of()))
+            .addComponent(new MovementComponent())
+            .addComponent(new CollisionComponent(false))
             .addComponent(new BombPlaceComponent())
+            .addComponent(new PowerUpListComponent(1, 1, List.of()))
             .addComponent(new DestroyComponent());  
     }
 
     @Override
     public Entity makePlayable(Pair<Float, Float> position) {
-        throw new UnsupportedOperationException("Unimplemented method 'makePlayable'");
+            return makeBomber(position, Type.PLAYABLE)
+                   .addComponent(new InputComponent());
+
     }
 
     @Override
     public Entity makeBot(Pair<Float, Float> position, int AI_difficulty) {
-        throw new UnsupportedOperationException("Unimplemented method 'makeBot'");
+        return makeBomber(position, Type.PLAYABLE)
+              .addComponent(new AIComponent());
     }
 
     @Override
     public Entity makeBomb(Entity placer) {
-        return new EntityImpl(game, placer.getPosition(), Type.BOMB)
-                    //TODO add other component
+        return new EntityImpl(game,placer.getPosition(), Type.BOMB)
+                   .addComponent(new MovementComponent())
+                   .addComponent(new CollisionComponent(true))
+                   .addComponent(new ExplodeComponent())
                    .addComponent(new PowerUpListComponent(placer))
                    .addComponent(new DestroyComponent());
     }
 
     @Override
     public Entity makeDestructibleWall(Pair<Float, Float> position) {
-        return new EntityImpl(game, position, Type.DESTRUCTIBLE_WALL)
-                    .addComponent(new DestroyComponent());
+        return new EntityImpl(game,position, Type.DESTRUCTIBLE_WALL)
+                   .addComponent(new DestroyComponent());
     }
 
     @Override
     public Entity makeIndestructibleWall(Pair<Float, Float> position) {
-        return new EntityImpl(game, position, Type.INDESTRUCTIBLE_WALL);
+        return new EntityImpl(game,position, Type.INDESTRUCTIBLE_WALL);
     }
     
 }
