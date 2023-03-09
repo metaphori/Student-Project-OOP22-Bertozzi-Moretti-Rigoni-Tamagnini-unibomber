ac5f63d4e2b6e48b7a1f972b2a9584e2806d81d9
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-03-09 15:47:52 +0100

    fixed movement and input component

--- src/main/java/it/unibo/unibomber/game/ecs/impl/InputComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/InputComponent.java
@@ -4,56 +4,78 @@ import java.util.Optional;
 import java.awt.event.KeyEvent;
 import it.unibo.unibomber.utilities.Pair;
 
-public class InputComponent extends AbstractComponent{
-
+public class InputComponent extends AbstractComponent {
+     final int NO_KEYS_VALUE = -1;
 
      @Override
      public void update() {
-          
-          var keyPressed=this.getEntity().getGame().getWorld().getPlay().getKeys();
-          Optional<Integer> moveKey = keyPressed.stream()
-                         .filter(e->e ==(int) KeyEvent.VK_W || e == (int)KeyEvent.VK_A || e == (int)KeyEvent.VK_S || e == (int)KeyEvent.VK_D)
-                         .findFirst();
-          Optional<Integer> spazio = keyPressed.stream()
-          .filter(e->e == (int)KeyEvent.VK_SPACE)
-          .findFirst();
-          if(moveKey.isPresent()){
-               Pair<Float,Float> moveBy= new Pair<Float,Float>(0f, 0f);
-               switch (moveKey.get()){
-                    case (int)KeyEvent.VK_W:
-                         moveBy = new Pair<Float,Float>(0f,-0.2f);
-                         break;
-                    case (int)KeyEvent.VK_A:
-                         moveBy = new Pair<Float,Float>(-0.2f,0f);
-                         break;
-                    case (int)KeyEvent.VK_S:
-                         moveBy = new Pair<Float,Float>(0f,0.2f);
-                         break;
-                    case (int) KeyEvent.VK_D:
-                         moveBy = new Pair<Float,Float>(0.2f,0f);
-                    break;
 
-               }
-               if(spazio.isPresent()){
-                    this.getEntity().getComponent(BombPlaceComponent.class).get().placeBomb();
-               }
-               var movementComponent = this.getEntity().getComponent(MovementComponent.class);
-               if(movementComponent.isPresent()){
-                    MovementComponent move = movementComponent.get();
-                    move.moveBy(moveBy);
-               }
-               
-          } 
+          Optional<Integer> moveKey = getMoveKey();
+          // TODO change to getFunctionalKeys when needed
+          Optional<Integer> spazio = getSpaceKey();
+          Pair<Float, Float> moveBy = calculateMovement(moveKey);
+
+          if (spazio.isPresent()) {
+               this.getEntity().getComponent(BombPlaceComponent.class).get().placeBomb();
+          }
+          updateMovement(moveBy);
      }
-     /*   
-        game.addkeyPressed(87);w
-        game.addkeyPressed(65);a
-     game.addkeyPressed(83);s
-        game.addkeyPressed(68);d
-        game.addkeyPressed(27);esc
-        game.addkeyPressed(32);space
-        game.addkeyPressed(37);left
-        game.addkeyPressed(39);right
+
+     /**
+      * @param moveBy how much the player should move
+      *               updates the movementComponent relative to this player
       */
-     
+     private void updateMovement(Pair<Float, Float> moveBy) {
+          var movementComponent = this.getEntity().getComponent(MovementComponent.class);
+
+          if (movementComponent.isPresent()) {
+               MovementComponent move = movementComponent.get();
+               move.moveBy(moveBy);
+          }
+     }
+
+     /**
+      * @param moveKey the last movement Key pressed
+      * @return how much the player should move given moveKey
+      */
+     private Pair<Float, Float> calculateMovement(Optional<Integer> moveKey) {
+          Integer keyValue = moveKey.isPresent() ? moveKey.get() : NO_KEYS_VALUE;
+          switch (keyValue) {
+               case KeyEvent.VK_W:
+                    return new Pair<Float, Float>(0f, -0.2f);
+               case KeyEvent.VK_A:
+                    return new Pair<Float, Float>(-0.2f, 0f);
+
+               case KeyEvent.VK_S:
+                    return new Pair<Float, Float>(0f, 0.2f);
+
+               case KeyEvent.VK_D:
+                    return new Pair<Float, Float>(0.2f, 0f);
+               default:
+                    return new Pair<Float, Float>(0f, 0f);
+          }
+     }
+
+     private Optional<Integer> getSpaceKey() {
+          var keyPressed = this.getEntity().getGame().getWorld().getPlay().getKeys();
+
+          return keyPressed.stream()
+                    .filter(e -> e == (int) KeyEvent.VK_SPACE)
+                    .findFirst();
+     }
+
+     /**
+      * @return the last key (relative to the 4 movement keys) the player pressed
+      */
+     private Optional<Integer> getMoveKey() {
+          var keyPressed = this.getEntity().getGame().getWorld().getPlay().getKeys();
+
+          return keyPressed.stream()
+                    .filter(e -> e == (int) KeyEvent.VK_W ||
+                              e == (int) KeyEvent.VK_A ||
+                              e == (int) KeyEvent.VK_S ||
+                              e == (int) KeyEvent.VK_D)
+                    .findFirst();
+     }
+
 }
--- src/main/java/it/unibo/unibomber/game/ecs/impl/MovementComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/MovementComponent.java
@@ -7,58 +7,63 @@ import it.unibo.unibomber.utilities.Pair;
 
 public class MovementComponent extends AbstractComponent {
 
-    private static float globalSpeedMultiplier=1;
-    private Pair<Float,Float> moveBy;
+    private static float globalSpeedMultiplier = 1;
+    private Pair<Float, Float> moveBy;
     private Direction direction = Direction.DOWN;
-    private int framesInDirection=0;
-    private int passedFrame=0;
-    public MovementComponent(){
-        moveBy = new Pair<Float,Float>(0f, 0f);
+    private int framesInDirection = 0;
+    private int passedFrame = 0;
+
+    public MovementComponent() {
+        moveBy = new Pair<Float, Float>(0f, 0f);
     }
+
     @Override
     public void update() {
         this.getEntity().addPosition(moveBy);
         handleDirection();
-        //checkCollisions();
+        System.out.println(direction.toString());
+        // checkCollisions();
     }
 
     private void handleDirection() {
-        Direction newDirection=Direction.extractDirecion(moveBy);
-        if(this.direction == newDirection){
+        Direction newDirection = Direction.extractDirecion(moveBy);
+        if (this.direction == newDirection) {
             this.framesInDirection++;
-        }
-        else{
-            this.framesInDirection=0;
+        } else {
+            this.framesInDirection = 0;
             this.direction = newDirection;
         }
-        if(framesInDirection == 10){
+        if (framesInDirection == 10) {
             passedFrame++;
-            this.framesInDirection=0;
+            this.framesInDirection = 0;
         }
     }
-    private void checkCollisions() {        
+
+    private void checkCollisions() {
         Optional<CollisionComponent> collisionComp = this.getEntity().getComponent(CollisionComponent.class);
-        if(collisionComp.isPresent()){
+        if (collisionComp.isPresent()) {
             collisionComp.get().checkCollisions();
         }
     }
-    public void moveBy(final Pair<Float,Float> moveBy){
-        this.moveBy=new Pair<>( moveBy.getX()*this.getEntity().getSpeed()*globalSpeedMultiplier,
-                                moveBy.getY()*this.getEntity().getSpeed()*globalSpeedMultiplier);
+
+    public void moveBy(final Pair<Float, Float> moveBy) {
+        this.moveBy = new Pair<>(moveBy.getX() * this.getEntity().getSpeed() * globalSpeedMultiplier,
+                moveBy.getY() * this.getEntity().getSpeed() * globalSpeedMultiplier);
     }
-    public Direction getDirection(){
+
+    public Direction getDirection() {
         return this.direction;
     }
-    public int getFrameInDirection(){
+
+    public int getFrameInDirection() {
         return this.framesInDirection;
     }
-    public int getPassedFram(){
+
+    public int getPassedFram() {
         return this.passedFrame;
     }
-    public void resetMoveBy() {
-        moveBy = new Pair<Float,Float>(0f, 0f);
-    }
-    public static void setGlobalSpeedMultiplier(float speed){
-        globalSpeedMultiplier=speed;
+
+    public static void setGlobalSpeedMultiplier(float speed) {
+        globalSpeedMultiplier = speed;
     }
 }
