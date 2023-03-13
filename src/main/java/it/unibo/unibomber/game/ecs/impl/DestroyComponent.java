b36fea5708f3d7db22eabb33038cea21746a4b13
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-13 13:36:08 +0100

    Generalized update method in DestroyComponent

--- src/main/java/it/unibo/unibomber/game/ecs/impl/DestroyComponent.java
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/DestroyComponent.java
@@ -8,7 +8,7 @@ import it.unibo.unibomber.game.ecs.api.PowerUpType;
 import it.unibo.unibomber.game.ecs.api.Type;
 import it.unibo.unibomber.utilities.Pair;
 
-import static it.unibo.unibomber.utilities.Constants.Destroy.DESTROY_DURATION;
+import static it.unibo.unibomber.utilities.Constants.Destroy.getDestructionFrames;
 import static it.unibo.unibomber.utilities.Constants.Destroy.DROPPED_POWERUP_PERCENT;
 
 /**
@@ -32,7 +32,7 @@ public final class DestroyComponent extends AbstractComponent {
     public void update() {
         if (this.isDestroyed) {
             this.destroyFrames++;
-            if (this.destroyFrames == DESTROY_DURATION) {
+            if (this.destroyFrames >= getDestructionFrames(this.getEntity().getType())) {
                 if (this.getEntity().getType() != Type.BOMB) {
                     dropPowerUps();
                 }
