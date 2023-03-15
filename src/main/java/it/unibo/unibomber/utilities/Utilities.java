e8447bbabcb9d3b2a2ccf9dc5a95a508593d6828
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-03-15 07:21:25 +0100

    Added "between" function

---
+++ src/main/java/it/unibo/unibomber/utilities/Utilities.java
@@ -0,0 +1,7 @@
+package it.unibo.unibomber.utilities;
+
+public class Utilities {
+    public static boolean isBetween(int value,int min, int max){
+        return (value >min && value<max);
+    }
+}
