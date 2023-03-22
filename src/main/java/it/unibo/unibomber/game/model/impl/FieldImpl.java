a58f010db0b8faf406b92e1350b1097a4ed57386
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-22 15:02:00 +0100

    Added control of entities in FieldImpl

--- src/main/java/it/unibo/unibomber/game/model/impl/FieldImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/FieldImpl.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import it.unibo.unibomber.game.ecs.api.Entity;
 import it.unibo.unibomber.game.ecs.api.Type;
 import it.unibo.unibomber.game.ecs.impl.PowerUpListComponent;
+import it.unibo.unibomber.game.ecs.impl.ThrowComponent;
 import it.unibo.unibomber.game.model.api.Field;
 import it.unibo.unibomber.game.model.api.Game;
 import it.unibo.unibomber.utilities.Constants;
@@ -49,10 +50,13 @@ public class FieldImpl implements Field {
                                             .collect(Collectors.toList());
         this.field.clear();
         for (final var entity : fieldentities) {
-            row = Math.round(entity.getPosition().getX());
-            col = Math.round(entity.getPosition().getY());
-            this.field.put(new Pair<Integer, Integer>(row, col),
-                    new Pair<Type, Entity>(entity.getType(), entity));
+            if (entity.getType() != Type.BOMB
+                || !entity.getComponent(ThrowComponent.class).get().getThrowing()) {
+                row = Math.round(entity.getPosition().getX());
+                col = Math.round(entity.getPosition().getY());
+                this.field.put(new Pair<Integer, Integer>(row, col),
+                        new Pair<Type, Entity>(entity.getType(), entity));
+            }
         }
     }
 
