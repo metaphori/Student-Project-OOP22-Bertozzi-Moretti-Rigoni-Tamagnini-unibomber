4b47215c9f061d0a799a0c7161c124f91020bd89
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-03-23 16:44:42 +0100

    Added abstractButton class as intermediate class

---
+++ src/main/java/it/unibo/unibomber/game/model/impl/AbstractMenuButton.java
@@ -0,0 +1,61 @@
+package it.unibo.unibomber.game.model.impl;
+
+import java.awt.Rectangle;
+
+import it.unibo.unibomber.game.model.api.MenuButton;
+import it.unibo.unibomber.utilities.Constants;
+
+/**
+ * an intermediate class for menu buttons.
+ */
+public abstract class AbstractMenuButton implements MenuButton {
+
+    protected boolean mouseOver, mousePressed;
+    protected final Rectangle bounds;
+    protected final int x, y, rowIndex;
+    protected final int xButtonPosition = Constants.UI.Buttons.getBWidht() / 2;
+
+    /**
+     * @param x x coordinate of the button.
+     * @param y y coordinate of the button.
+     * @param rowIndex index which represents the button row.
+     */
+    public AbstractMenuButton(final int x, final int y, final int rowIndex) {
+        this.x = x;
+        this.y = y;
+        this.rowIndex = rowIndex;
+        bounds = new Rectangle(x - xButtonPosition, y, Constants.UI.Buttons.getBWidht(),
+                Constants.UI.Buttons.getBHeight());
+    }
+
+    @Override
+    public final boolean isMouseOver() {
+        return mouseOver;
+    }
+
+    @Override
+    public final void setMouseOver(final boolean mouseOver) {
+        this.mouseOver = mouseOver;
+    }
+
+    @Override
+    public final boolean isMousePressed() {
+        return mousePressed;
+    }
+
+    @Override
+    public final void setMousePressed(final boolean mousePressed) {
+        this.mousePressed = mousePressed;
+    }
+
+    @Override
+    public final Rectangle getBounds() {
+        return new Rectangle(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+
+    @Override
+    public final void reset() {
+        mouseOver = false;
+        mousePressed = false;
+    }
+}
--- src/main/java/it/unibo/unibomber/game/model/impl/OptionButtonImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/OptionButtonImpl.java
@@ -1,25 +1,19 @@
 package it.unibo.unibomber.game.model.impl;
 
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import it.unibo.unibomber.game.controller.api.GameLoop;
 import it.unibo.unibomber.game.model.api.Gamestate;
-import it.unibo.unibomber.game.model.api.MenuButton;
-import it.unibo.unibomber.utilities.Constants;
 import it.unibo.unibomber.utilities.UploadRes;
 
 /**
  * Menu Button settings implementation class.
  */
-public class OptionButtonImpl implements MenuButton, GameLoop {
-  private final int x, y, width, height, rowIndex;
-  private final int xButtonPosition = Constants.UI.Buttons.getBWidht() / 2;
+public class OptionButtonImpl extends AbstractMenuButton implements GameLoop {
+  private final int width, height;
   private final String type;
   private BufferedImage[] bufferImages;
-  private boolean mouseOver, mousePressed;
-  private final Rectangle bounds;
 
   /**
    * @param x
@@ -30,14 +24,11 @@ public class OptionButtonImpl implements MenuButton, GameLoop {
    * @param type
    */
   public OptionButtonImpl(final int x, final int y, final int rowIndex, final int w, final int h, final String type) {
-    this.x = x;
-    this.y = y;
+    super(x, y, rowIndex);
     this.width = w;
     this.height = h;
-    this.rowIndex = rowIndex;
     this.type = type;
     loadbufferImages();
-    bounds = new Rectangle(x - xButtonPosition, y, Constants.UI.Buttons.getBWidht(), Constants.UI.Buttons.getBHeight());
   }
 
   private void loadbufferImages() {
@@ -58,31 +49,6 @@ public class OptionButtonImpl implements MenuButton, GameLoop {
   public void update() {
   }
 
-  @Override
-  public final boolean isMouseOver() {
-    return mouseOver;
-  }
-
-  @Override
-  public final void setMouseOver(final boolean mouseOver) {
-    this.mouseOver = mouseOver;
-  }
-
-  @Override
-  public final boolean isMousePressed() {
-    return mousePressed;
-  }
-
-  @Override
-  public final void setMousePressed(final boolean mousePressed) {
-    this.mousePressed = mousePressed;
-  }
-
-  @Override
-  public final Rectangle getBounds() {
-    return bounds;
-  }
-
   /**
    * setup game based on settings.
    */
@@ -90,11 +56,5 @@ public class OptionButtonImpl implements MenuButton, GameLoop {
     if ("ok".equals(type)) {
       Gamestate.setGameState(Gamestate.PLAY);
     }
-  }
-
-  @Override
-  public final void reset() {
-    mouseOver = false;
-    mousePressed = false;
   }
 }
