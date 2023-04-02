b0f000cfbb84df2b79f91b8d380b969bd51fef1d
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-04-02 19:40:18 +0200

    bugfixing

--- src/main/java/it/unibo/unibomber/inputs/KeyboardInputsImpl.java
+++ src/main/java/it/unibo/unibomber/inputs/KeyboardInputsImpl.java
@@ -10,50 +10,50 @@ import it.unibo.unibomber.game.view.WorldPanelImpl;
  * KeyboardInputsImpl class.
  */
 public final class KeyboardInputsImpl implements KeyListener {
- private final WorldPanelImpl worldPanel;
+  private final WorldPanelImpl worldPanel;
 
- /**
-  * KeyboardInputsImpl constructor.
-  * 
-  * @param worldPanel
-  */
- public KeyboardInputsImpl(final WorldPanelImpl worldPanel) {
-  this.worldPanel = worldPanel;
- }
-
- @Override
- public void keyPressed(final KeyEvent e) {
-  switch (Gamestate.getGamestate()) {
-   case MENU:
-    break;
-   case PLAY:
-    worldPanel.getWorld().getPlay().keyPressed(e);
-    break;
-    case PAUSE:
-    worldPanel.getWorld().getEndGame().keyPressed(e);
-    break;
-   default:
-    break;
+  /**
+   * KeyboardInputsImpl constructor.
+   * 
+   * @param worldPanel
+   */
+  public KeyboardInputsImpl(final WorldPanelImpl worldPanel) {
+    this.worldPanel = worldPanel;
   }
- }
 
- @Override
- public void keyReleased(final KeyEvent e) {
-  switch (Gamestate.getGamestate()) {
-   case MENU:
-    break;
-   case PLAY:
-    worldPanel.getWorld().getPlay().keyReleased(e);
-    break;
-    case PAUSE:
-    worldPanel.getWorld().getEndGame().keyReleased(e);
-    break;
-   default:
-    break;
+  @Override
+  public void keyPressed(final KeyEvent e) {
+    switch (Gamestate.getGamestate()) {
+      case MENU:
+        break;
+      case PLAY:
+        worldPanel.getWorld().getPlay().keyPressed(e);
+        break;
+      case PAUSE:
+        worldPanel.getWorld().getEndGame().keyPressed(e);
+        break;
+      default:
+        break;
+    }
   }
- }
 
- @Override
- public void keyTyped(final KeyEvent e) {
- }
+  @Override
+  public void keyReleased(final KeyEvent e) {
+    switch (Gamestate.getGamestate()) {
+      case MENU:
+        break;
+      case PLAY:
+        worldPanel.getWorld().getPlay().keyReleased(e);
+        break;
+      case PAUSE:
+        worldPanel.getWorld().getEndGame().keyReleased(e);
+        break;
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void keyTyped(final KeyEvent e) {
+  }
 }
